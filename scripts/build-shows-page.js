fbdbc2401c6dc2949ef6652daa63c74c871e8357
astha vohra <vohraastha.2712@gmail.com>
2022-02-12 12:54:00 +0000

    simplified dom for shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -30,6 +30,77 @@ let shows = [
     location: "San Francisco, CA",
   },
 ];
+
+let showDetailsClasses = ["show__date", "show__venue", "show__location"];
+let showContainer = document.querySelector(".shows__list");
+
+const displayShowHeaders = () => {
+  let showHeaderTexts = ["DATE", "VENUE", "LOCATION"];
+  let showHeaderList = document.createElement("ul");
+  showHeaderList.classList.add("shows__labels-tablet");
+
+  showHeaderTexts.forEach((labels) => {
+    let showLabel = document.createElement("li");
+    showLabel.classList.add("shows__label");
+    showLabel.innerText = labels;
+    showHeaderList.appendChild(showLabel);
+  });
+
+  showContainer.appendChild(showHeaderList);
+};
+const displayShowDetails = (show) => {
+  let showClassCounter = 0;
+  let showDiv = document.createElement("div");
+  showDiv.classList.add("show");
+  showDiv.setAttribute("onclick", "markActive(event)");
+
+  let showLabels = document.createElement("ul");
+  showLabels.classList.add("show__details");
+
+  Object.keys(show).forEach((key) => {
+    let showLabel = document.createElement("li");
+    showLabel.classList.add("show__label--mobile");
+    showLabel.innerText = key;
+
+    let showLabelValue = document.createElement("li");
+    showLabelValue.classList.add(showDetailsClasses[showClassCounter++]);
+    showLabelValue.innerText = show[key];
+
+    showLabels.appendChild(showLabel);
+    showLabels.appendChild(showLabelValue);
+    showClassCounter = showClassCounter % 3;
+  });
+  showDiv.appendChild(showLabels);
+
+  let showButton = document.createElement("button");
+  showButton.classList.add("show__cta");
+  showButton.innerText = "BUY TICKETS";
+  showDiv.appendChild(showButton);
+  showContainer.appendChild(showDiv);
+};
+
+const markActive = (event) => {
+  let showDivs = showContainer.querySelectorAll("div");
+  let currentDiv = event.target;
+  let showParent = currentDiv;
+  showDivs.forEach(function (show) {
+    show.classList.remove("show__selected");
+  });
+  if (currentDiv.localName === "li") {
+    showParent = currentDiv.parentElement.parentElement;
+  } else if (currentDiv.localName === "ul") {
+    showParent = currentDiv.parentElement;
+  }
+  showParent.classList.add("show__selected");
+};
+
+displayShowHeaders();
+
+// Display the entire shows array
+shows.forEach((show) => {
+  displayShowDetails(show);
+});
+/*
 // initialize an array to hold each labels for each show detail
 let labels = ["DATE", "VENUE", "LOCATION"];
 
@@ -127,3 +198,4 @@ const displayShow = (show) => {
 shows.forEach((show) => {
   displayShow(show);
 });
+*/
